99f04492bd83b2492ec36929b101c15911e328b2
Krasimir Petranov <kimim0548@gmail.com>
2025-03-29 12:03:12 +0000

    Add footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,86 @@
+export default function Footer() {
+    return (
+        <>
+        <div className="section_footer ">
+        <div className="container">
+          <div className="footer_section_2">
+            <div className="row">
+              <div className="col-sm-6 col-md-6 col-lg-3">
+                <h2 className="account_text">About Us</h2>
+                <p className="ipsum_text_2">
+                  dolor sit amet, consectetur magna aliqua. Ut enim ad minim
+                  veniam, quisdotempor incididunt r
+                </p>
+              </div>
+              <div className="col-sm-6 col-md-6 col-lg-3">
+                <h2 className="account_text">Useful Link</h2>
+                <div className="useful_link">
+                  <ul>
+                    <li>
+                      <a href="#">Video games</a>
+                    </li>
+                    <li>
+                      <a href="#">Remote control</a>
+                    </li>
+                    <li>
+                      <a href="#">3d controller</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-sm-6 col-md-6 col-lg-3">
+                <h2 className="account_text">Contact Us</h2>
+                <p className="ipsum_text_2">
+                  dolor sit amet, consectetur magna aliqua. quisdotempor
+                  incididunt ut e{" "}
+                </p>
+              </div>
+              <div className="col-sm-6 col-md-6 col-lg-3">
+                <h2 className="account_text">Newsletter</h2>
+                <input
+                  type=""
+                  className="email_text"
+                  placeholder="Enter Your Email"
+                  name="Enter Your Email"
+                />
+                <button className="subscribr_bt">SUBSCRIBE</button>
+              </div>
+            </div>
+          </div>
+          <div className="social_icon">
+            <ul>
+              <li>
+                <a href="#">
+                  <img src="images/fb-icon.png" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="images/twitter-icon.png" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="images/linkdin-icon.png" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="images/instagram-icon.png" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="copyright_section">
+          <div className="container">
+            <p className="copyright_text">
+              Copyright 2020 All Right Reserved By{" "}
+              <a href="https://html.design/" /> Free html Templates
+            </p>
+          </div>
+        </div>
+        </>
+    )
+}
