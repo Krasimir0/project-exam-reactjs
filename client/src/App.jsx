fe636d652cf8ea03cdb115ad3b0975452ebab154
Krasimir Petranov <kimim0548@gmail.com>
2025-03-29 14:08:27 +0000

    Initialize react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
-import Header from './components/header/Header'
-import Home from './components/header/home/Home'
 import { Routes, Route } from 'react-router'
+
+import Catalog from './components/catalog/Catalog'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import Login from './components/login/Login'
+import Register from './components/register/Register'
+import CreateDevice from './components/create-device/CreateDevice'
+
 function App() {
 
   return (
@@ -8,6 +14,10 @@ function App() {
     <Header />
     <Routes>
        <Route path='/' element={<Home />}/> 
+       <Route path='/devices' element={<Catalog />} />
+       <Route path='/devices/create' element={<CreateDevice />} />
+       <Route path='/login' element={<Login />} />
+       <Route path='/register' element={<Register />} />       
     </Routes>
         
     </>
