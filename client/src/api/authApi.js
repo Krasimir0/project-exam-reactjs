f8d7824f988391a82058bc7a525311babcb4ede6
Krasimir Petranov <kimim0548@gmail.com>
2025-03-31 17:54:17 +0100

    Add logout functionality

---
+++ client/src/api/authApi.js
@@ -0,0 +1,53 @@
+import { useContext, useEffect } from "react"
+import { UserContext } from "../contexts/UserContext"
+import request from "../utils/request"
+
+
+const baseUrl = "http://localhost:3030/users"
+
+
+export const useLogin = () => {
+    const login = (email, password) => 
+        request.post(`${baseUrl}/login`, { email, password })
+    
+
+    return {
+        login
+    }
+}
+
+export const useRegister = () => {
+    const register = (email, password) => 
+        request.post(`${baseUrl}/register`, { email, password });
+    
+
+    return {
+        register
+    }
+}
+
+export const useLogout = () => {
+    const { accessToken, userLogoutHandler } = useContext(UserContext);
+    console.log(accessToken);
+    
+
+    useEffect(() => {
+        if (!accessToken) {
+            return;
+        }
+
+        const options = {
+            headers: {
+                'X-Authorization': accessToken,
+            }
+        };
+        request.get(`${baseUrl}/logout`, null,  options)
+        .finally(userLogoutHandler);
+
+    }, [accessToken, userLogoutHandler]);
+    
+    
+    return {
+        isLoggedOut: !!accessToken,
+    }
+};
