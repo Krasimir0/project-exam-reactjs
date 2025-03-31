c140d5131b97bd27a2a463ba2048a847b5e12d7a
Krasimir Petranov <kimim0548@gmail.com>
2025-03-31 17:52:24 +0100

    Add user provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,35 @@
 import { Routes, Route } from 'react-router'
 
-import Catalog from './components/catalog/Catalog'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Login from './components/login/Login'
 import Register from './components/register/Register'
 import CreateDevice from './components/create-device/CreateDevice'
+import Catalog from './components/catalog-device/Catalog'
+import Logout from './components/logout/Logout'
+import AuthGuard from './components/guards/AuthGuard'
+import GuestGuard from './components/guards/GuestGuard'
+import UserProvider from './providers/UserProvider'
 
 function App() {
 
   return (
     <>
+    <UserProvider>
     <Header />
     <Routes>
        <Route path='/' element={<Home />}/> 
        <Route path='/devices' element={<Catalog />} />
-       <Route path='/devices/create' element={<CreateDevice />} />
-       <Route path='/login' element={<Login />} />
-       <Route path='/register' element={<Register />} />       
+       <Route element={<AuthGuard />}>
+        <Route path='/devices/create' element={<CreateDevice />} />
+        <Route path='/logout' element={<Logout />} />
+       </Route>
+       <Route element={<GuestGuard />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+       </Route>       
     </Routes>
-        
+    </UserProvider> 
     </>
   )
 }
