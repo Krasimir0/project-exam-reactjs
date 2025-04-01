dcc87b20871c0fcb77121aa43b844b91a197ef85
Krasimir Petranov <kimim0548@gmail.com>
2025-04-01 23:16:18 +0100

    Get the latest devices to home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router";
+import { useLatestDevices } from "../../api/deviceApi";
 import Footer from "../footer/Footer";
 
 export default function Home() {
+  const { latestDevices } = useLatestDevices();
+
   return (
     <>
       <div className="banner_section layout_padding">
@@ -14,60 +18,13 @@ export default function Home() {
               <div className="carousel-item active">
                 <div className="row">
                   <div className="col-md-6">
-                    <h1 className="video_text">Video games</h1>
-                    <h1 className="controller_text">controller</h1>
+                    <h1 className="video_text">Level Up Your Gaming Experience!</h1>
+                    <h1 className="controller_text">Next-Gen Devices</h1>
                     <p className="banner_text">
-                      There are many variations of passages of Lorem Ipsum
-                      available, but the majority have suffered alteration in
-                      some form, by injected humour, or randomised words which
-                      don't look even slightly believable
+                    Discover cutting-edge gaming devices designed for performance and comfort. Elevate your gameplay with responsive controls and immersive features tailored for every gamer.
                     </p>
                     <div className="shop_bt">
-                      <a href="#">Shop Now</a>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="image_1">
-                      <img src="images/img-1.png" alt="controller" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="row">
-                  <div className="col-md-6">
-                    <h1 className="video_text">Video games</h1>
-                    <h1 className="controller_text">controller</h1>
-                    <p className="banner_text">
-                      There are many variations of passages of Lorem Ipsum
-                      available, but the majority have suffered alteration in
-                      some form, by injected humour, or randomised words which
-                      don't look even slightly believable
-                    </p>
-                    <div className="shop_bt">
-                      <a href="#">Shop Now</a>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="image_1">
-                      <img src="images/img-1.png" alt="controller" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="row">
-                  <div className="col-md-6">
-                    <h1 className="video_text">Video games</h1>
-                    <h1 className="controller_text">controller</h1>
-                    <p className="banner_text">
-                      There are many variations of passages of Lorem Ipsum
-                      available, but the majority have suffered alteration in
-                      some form, by injected humour, or randomised words which
-                      don't look even slightly believable
-                    </p>
-                    <div className="shop_bt">
-                      <a href="#">Shop Now</a>
+                      <Link to={'/devices'}>See more</Link>
                     </div>
                   </div>
                   <div className="col-md-6">
@@ -78,22 +35,6 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <a
-              className="carousel-control-prev"
-              href="#my_slider"
-              role="button"
-              data-bs-slide="prev"
-            >
-              <i className="fa fa-angle-left"></i>
-            </a>
-            <a
-              className="carousel-control-next"
-              href="#my_slider"
-              role="button"
-              data-bs-slide="next"
-            >
-              <i className="fa fa-angle-right"></i>
-            </a>
           </div>
         </div>
       </div>
@@ -106,15 +47,12 @@ export default function Home() {
               </div>
             </div>
             <div className="col-md-6">
-              <h1 className="about_text">ABOUT</h1>
+              <h1 className="about_text">ABOUT US</h1>
               <p className="lorem_text">
-                There are many variations of passages of Lorem Ipsum available,
-                but the majority have suffered alteration in some form, by
-                injected humour, or randomised words which don't look even
-                slightly believable
+              we are passionate about enhancing your gaming experience. From high-performance accessories to the latest gaming gadgets, we bring you the best in innovation and quality. Step into the future of gaming with us!
               </p>
               <div className="shop_bt_2">
-                <a href="#">Shop Now</a>
+                <Link to={'/devices'}>Explore Our Collection</Link>
               </div>
             </div>
           </div>
@@ -123,60 +61,53 @@ export default function Home() {
       <div className="product_section layout_padding">
         <div className="container">
           <div className="product_text">
-            Our <span style={{ color: "#5ca0e9" }}>products</span>
+            Our <span style={{ color: "#5ca0e9" }}>Our Latest Products</span>
           </div>
           <p className="long_text">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem
+          Browse through our collection of high-tech gaming devices, from controllers to VR gear. Designed for gamers, by gamers.
           </p>
-          <div className="product_section_2">
-            <div className="row">
-              <div className="col-md-6">
-                <div className="image_2">
-                  <img src="images/img-3.png" alt="product" />
+          {latestDevices.map((device) => (
+            <div className="product_section_2" key={device._id}>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="image_2">
+                    <img src={device.imageUrl} alt="product" />
+                  </div>
+                  <div className="price_text">
+                    Price ${" "}
+                    <span style={{ color: "#3a3a38" }}>{device.price}</span>
+                  </div>
+                  <h1 className="game_text">{device.deviceType}</h1>
+                  <p className="long_text">{device.description}</p>
                 </div>
-                <div className="price_text">
-                  Price $ <span style={{ color: "#3a3a38" }}>200</span>
-                </div>
-                <h1 className="game_text">Video Game</h1>
-                <p className="long_text">
-                  It is a long established fact that a reader will be distracted
-                  by the readable content of a page when looking at its layout.
-                  The point of using Lorem
-                </p>
-              </div>
-              <div className="col-md-6">
-                <div className="image_2">
-                  <img src="images/img-3.png" alt="product" />
-                </div>
-                <div className="price_text">
-                  Price $ <span style={{ color: "#3a3a38" }}>300</span>
-                </div>
-                <h1 className="game_text">Video Game</h1>
-                <p className="long_text">
-                  It is a long established fact that a reader will be distracted
-                  by the readable content of a page when looking at its layout.
-                  The point of using Lorem
-                </p>
               </div>
             </div>
-          </div>
+          ))}
+          {latestDevices.length === 0 && (
+            <p
+              style={{
+                fontSize: "36px", // Larger text size
+                fontWeight: "bold", // Make the text bold
+                textAlign: "center", // Center the text
+                color: "black", // Eye-catching red color (you can choose another color)
+                padding: "20px 0", // Add some space around the text
+                fontFamily: "'Arial', sans-serif", // Modern font
+              }}
+            >
+              No devices yet
+            </p>
+          )}
+
           <div className="see_main">
             <div className="see_bt">
-              <a href="#">See More</a>
+              <Link to='/devices'>See More</Link>
             </div>
           </div>
         </div>
       </div>
       <div className="testimonial_section layout_padding">
         <div className="container">
-          <h1 className="testimonial_text">Reviews</h1>
-          <p className="ipsum_text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud
-          </p>
+          <h1 className="testimonial_text">What Gamers Say About Us</h1>
           <div className="testimonial_section_2">
             <div className="box_main">
               <div className="quote_icon">
@@ -184,11 +115,7 @@ export default function Home() {
               </div>
               <p className="dolor_text">
                 {" "}
-                dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum{" "}
+                {"The best devices I've ever used! The grip, responsiveness, and design are top-notch. Highly recommend for serious gamers! - Alex M."}{" "}
               </p>
               <div className="client_main">
                 <div className="client_left">
