bcb8a8d917f3dcff4f60637b99d11b4ae2e6bb13
Krasimir Petranov <kimim0548@gmail.com>
2025-04-01 23:17:10 +0100

    Add functionality for create game

--- client/src/components/create-device/CreateDevice.jsx
+++ client/src/components/create-device/CreateDevice.jsx
@@ -1,47 +1,53 @@
+import { useNavigate } from 'react-router' 
+import { useCreateGame } from '../../api/deviceApi';
+
 export default function CreateDevice() {
-   
+      const navigate = useNavigate();
+      const { create } = useCreateGame();
+
+      const submitAction = async (formData) => {
+          const deviceData = Object.fromEntries(formData);
+          
+          await create(deviceData);
+
+          navigate('/devices')          
+      }
     
       return (
         <div className="create-device-form">
           <h2>Create New Device</h2>
-          <form>
+          <form action={submitAction}>
             <input
               type="text"
               name="deviceName"
               placeholder="Device Name"
-            //   value={}
-            //   onChange={handleChange}
-            //   required
+              required
             />
             <select name="deviceType">
               <option value="">Select Device Type</option>
               <option value="mobile">Mobile</option>
               <option value="laptop">Laptop</option>
               <option value="tablet">Tablet</option>
+              <option value="tablet">Console</option>
+              <option value="tablet">PC</option>
             </select>
             <textarea
               name="description"
               placeholder="Device Description"
               rows="4"
-            //   value={formData.description}
-            //   onChange={handleChange}
-            //   required
+              required
             ></textarea>
             <textarea
               name="price"
               placeholder="Price $"
               rows="1"
-            //   value={formData.description}
-            //   onChange={handleChange}
-            //   required
+              required
             ></textarea>
              <textarea
               name="imageUrl"
               placeholder="Image"
               rows="1"
-            //   value={formData.description}
-            //   onChange={handleChange}
-            //   required
+              required
             ></textarea>
             <button type="submit">Create Device</button>
           </form>
